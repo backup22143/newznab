f97bad5ca70ba0cb1d231c6dcdf0c3744524b8c2
blian <brian@bce.net>
2013-02-28 22:34:30 -0500

    Update backfill-incremental.sh
    Stop going back in time after we reach one year.  Stop everything and run optimise_db once a day.

--- backfill-incremental.sh
+++ backfill-incremental.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
-
-# SET THIS to how many days back you already have info for
-# no harm running it for days that you already have backfilled for
-# there will just be no new articles to download, but the next time 
-# through the loop it will go back 24 hours more
-daysago=8
-
+daysago=0
+maxdays=356
+TODAY=`date +%Y%m%d`
+optimisef=/tmp/optimise.${TODAY}
 IFS="
 "
 while :;
 do
-	mydate=`date --date="${daysago} days ago" +"%Y-%m-%d"`
-	echo "--------------------------------------------------------"
-	echo "backfilling $group to $mydate"
-	cd /var/www/newznab/misc/update_scripts
-	php backfill_date.php $mydate
-	php update_binaries.php
-	php update_releases.php
-	echo "sleeping for 30 min"
-	sleep 1800
-	daysago=`expr $daysago + 1`
+        chmod -R 777  /var/www/newznab/nzbfiles
+        mydate=`date --date="${daysago} days ago" +"%Y-%m-%d"`
+        echo "--------------------------------------------------------"
+        echo "backfilling $group to $mydate"
+        cd /var/www/newznab/misc/update_scripts
+        php backfill_date.php $mydate
+        php update_binaries.php
+        php update_releases.php
+        if [ ! -f $otimisef ] ; then
+                php optimise_db.php
+                touch $otimisef
+        fi
+        #echo "sleeping for 30 min"
+        #sleep 1800
+        echo "sleeping for 30 sec"
+        sleep 30
+        if [ $daysago -lt $maxdays ] ; then
+                daysago=`expr $daysago + 1`
+        else
+                daysago=1
+        fi
 done
